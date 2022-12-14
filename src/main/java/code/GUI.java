54e152ae1b18641a20535d4dec2bee1e95daeec4
Ioannis <iwannis123@outlook.com.gr>
2022-12-14 17:29:06 +0200

    Starting with Java Swing (Graphics User Interface).

---
+++ src/main/java/code/GUI.java
@@ -0,0 +1,14 @@
+package code;
+
+import javax.swing.*;
+
+public class GUI {
+    private JPanel panelMain;
+    private JButton loginButton;
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+    private JButton resetButton;
+    private JPanel buttonsPanel;
+    private JPanel panel;
+    private JLabel infoLabel;
+}
